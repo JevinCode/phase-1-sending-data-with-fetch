217f473032338d0d580ece08050c4c9557a0045c
Jevin <therealjevin@gmail.com>
2023-01-16 21:06:21 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+
+function submitData(userName, userEmail) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        const body = document.querySelector('body');
+        body.append(data.id);
+    })
+    .catch(error => {
+        const body = document.querySelector('body');
+        body.append(error.message);
+    });
+}
